dbccfbcb669c44bc30c2ffb26a04b716acc0dfba
Natasha Petrus <hello@natashapetrus.com>
2017-09-23 05:48:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,27 +17,16 @@ function nowServing(katzDeliLine){
   }
 }
 
-//function currentLine(line){
-//  if (line.length == 0){
-//    return 'The line is currently empty.';
-//  }
-//  else{
-//    let newString = 'The line is currently: ';
-//    for (let i=0; i<line.length-1; i++){
-//      newString = newString + `${[i+1]}. ${line[i]}, `;
-//    }
-//    newString = newString + `${line.length}. ${line[line.length-1]}`;
-//    return newString;
-//  }
-function currentLine(line) {
-var linePosition = line.length;
-
-if(linePosition == 0) {
-  return "The line is currently empty."
-}
-var string = "The line is currently: "
-for (var i = 1; i < line.length; i++){
-  string = string + `${i}. ${line[i - 1]}, `;
-}
-return string
+function currentLine(line){
+  if (line.length == 0){
+    return 'The line is currently empty.';
+  }
+  else{
+    let newString = 'The line is currently: ';
+    for (let i=0; i<line.length-1; i++){
+      newString = newString + `${[i+1]}. ${line[i]}, `;
+    }
+    newString = newString + `${line.length}. ${line[line.length-1]}`;
+    return newString;
+  }
 }
